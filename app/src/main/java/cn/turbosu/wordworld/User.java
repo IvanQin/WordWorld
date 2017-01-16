4581fc714905f31d240323e4881231205e7962ca
Qinyifan <yifanq@kth.se>
2017-01-17 00:47:01 +0800

    Add: add User class, describe the User features like email,password....

---
+++ app/src/main/java/cn/turbosu/wordworld/User.java
@@ -0,0 +1,12 @@
+package cn.turbosu.wordworld;
+
+/**
+ * Created by Qinyifan on 17/1/17.
+ */
+
+public class User {
+    String userName;
+    String userEmail;
+    String password;
+    boolean isVip;
+}
